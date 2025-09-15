cef5661f8a2ed13435f5e3b78a99219401dd3bcf
ummataliyev <djohnummataliyev@gmail.com>
2025-09-16 02:33:49 +0500

    added comment to repositry class

--- src/repositories/__init__.py
+++ src/repositories/__init__.py
@@ -1,4 +1,5 @@
 """
 Initialize repositories
 """
+
 from .user import * # noqa
--- src/repositories/user.py
+++ src/repositories/user.py
@@ -1,5 +1,5 @@
 """
-User repository
+Repository for User model.
 """
 
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -9,5 +9,21 @@ from src.interfaces.repository import BaseRepository
 
 
 class UserRepository(BaseRepository[User]):
+    """
+    Repository class for performing async database operations on the User model.
+
+    Inherits:
+        - BaseRepository[User]: Provides generic CRUD and query operations
+          for the User entity.
+
+    Attributes:
+        db_session (AsyncSession): Async SQLAlchemy session used for database access.
+    """
+
     def __init__(self, db_session: AsyncSession):
+        """
+        Initialize the UserRepository with an async database session.
+
+        :param db_session: Async SQLAlchemy session for User model operations.
+        """
         super().__init__(db_session, User)
