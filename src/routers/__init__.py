e8ca92854708a85b7841e80acc038b6ffe1f8c4d
ummataliyev <djohnummataliyev@gmail.com>
2025-09-16 02:38:17 +0500

    refactored API routers && added comments && cleaned unused logics 🪄

--- src/routers/__init__.py
+++ src/routers/__init__.py
@@ -1,6 +1,7 @@
 """
 Initialize routers
 """
+
 from fastapi import Request
 from fastapi import APIRouter
 from fastapi.templating import Jinja2Templates
@@ -12,8 +13,18 @@ home_router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
 
-@routers.get("/", response_description="Homepage", include_in_schema=False)
+@routers.get(
+        path="/", 
+        response_description="Homepage",
+        include_in_schema=False
+)
 async def home(request: Request):
+    """
+    Render the homepage using Jinja2 templates.
+
+    :param request: FastAPI Request object.
+    :return: HTML response for the homepage.
+    """
     return templates.TemplateResponse(
         name="index.html",
         context={"request": request, "github_username": "ummataliyev"}
